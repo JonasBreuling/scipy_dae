994ad394caf1cff854b7e8cded4fe3098f2b4e12
Jonas Breuling <jonas.breuling@inm.uni-stuttgart.de>
2024-04-18 18:53:56 +0200

    Nonlinear index 2 example of Jay can be solved with Radau and BDF.

---
+++ PyDAE/jay1993.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+from scipy.integrate import solve_ivp
+from scipy.sparse import eye
+from scipy.optimize._numdiff import approx_derivative
+from dae import BDF, Radau
+
+
+def generate_Jay1993():
+    def fun(t, y):
+        """Index2 DAE found in Jay1993 Example 7.
+
+        References:
+        -----------
+        Jay1993: https://link.springer.com/article/10.1007/BF01990349
+        """
+        y1, y2, z = y
+
+        dy = np.array(
+            [
+                y1 * y2**2 * z**2,
+                y1**2 * y2**2 - 3 * y2**2 * z,
+                y1**2 * y2 - 1.0,
+            ],
+            dtype=y.dtype,
+        )
+
+        return dy
+
+    # the jacobian is computed via finite-differences
+    def jac(t, x):
+        return approx_derivative(
+            fun=lambda x: fun(t, x), x0=x, method="cs", rel_step=1e-50
+        )
+
+    def plot(t, y):
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots()
+
+        ax.plot(t, y[0], "--r", label="y1")
+        ax.plot(t, y[1], "--g", label="y2")
+        ax.plot(t, y[2], "--b", label="z")
+
+        ax.plot(t, np.exp(t), "-r", label="y1 true")
+        ax.plot(t, np.exp(-2 * t), "-g", label="y2 true")
+        ax.plot(t, np.exp(2 * t), "-b", label="z true")
+
+        ax.grid()
+        ax.legend()
+
+        plt.show()
+
+    # construct singular mass matrix
+    mass_matrix = eye(3, format="csr")
+    mass_matrix[-1, -1] = 0
+
+    # DAE index
+    var_index = [0, 2, 2]
+
+    # initial conditions
+    y0 = np.ones(3, dtype=float)
+
+    # tolerances and t_span
+    rtol = atol = 1.0e-6
+    t_span = (0, 20)
+
+    return y0, mass_matrix, var_index, fun, jac, rtol, atol, t_span, plot
+
+
+if __name__ == "__main__":
+    y0, mass_matrix, var_index, fun, jac, rtol, atol, t_span, plot = generate_Jay1993()
+
+    sol = solve_ivp(
+        fun=fun,
+        t_span=t_span,
+        y0=y0,
+        rtol=rtol,
+        atol=atol,
+        jac=jac,
+        method=BDF,
+        # method=Radau,
+        mass_matrix=mass_matrix,
+        var_index=var_index,
+    )
+    print(f"sol: {sol}")
+
+    nfev = sol.nfev
+    njev = sol.njev
+    nlu = sol.nlu
+    success = sol.success
+    assert success
+
+    plot(sol.t, sol.y)
