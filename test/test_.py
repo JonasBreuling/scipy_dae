184f195909ad7f1e859ac58f369a89176f5ef59d
Jonas Breuling <jonas.breuling@inm.uni-stuttgart.de>
2024-06-10 15:43:11 +0200

    Maybe it is a scipy version problem?

--- test/test_.py
+++ test/test_.py
@@ -1,12 +1,12 @@
-import pytest
+# import pytest
 
-def test_dummy1():
-    assert 1 == 1
-    assert 2 > 1
+# def test_dummy1():
+#     assert 1 == 1
+#     assert 2 > 1
 
-def test_dummy2():
-    assert True
+# def test_dummy2():
+#     assert True
 
-if __name__ == "__main__":
-    test_dummy1()
-    test_dummy2()
+# if __name__ == "__main__":
+#     test_dummy1()
+#     test_dummy2()
