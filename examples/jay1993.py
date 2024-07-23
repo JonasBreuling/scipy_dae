eb41d00b724c230054462b45d264e0029aacaf04
Jonas Breuling <jonas.breuling@inm.uni-stuttgart.de>
2024-07-23 09:44:19 +0200

    Modified example of Jay with linear Lagrange multiplier.

--- examples/jay1993.py
+++ examples/jay1993.py
@@ -1,24 +1,24 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.testing import assert_allclose
 from scipy_dae.integrate import solve_dae, consistent_initial_conditions
 
 
-"""Index2 DAE found in Jay1993 Example 7.
+"""Modified index2 DAE found in Jay1993 Example 7.
 
 References:
 -----------
 Jay1993: https://link.springer.com/article/10.1007/BF01990349
 """
-def F(t, y, yp):
+def F(t, y, yp, nonlinear_multiplier=False):
     y1, y2, _ = y
     y1p, y2p, zp = yp
 
     F = np.zeros(3, dtype=y.dtype)
-    # TODO: This multiplier enters nonlinear. Can we cope with that?
-    F[0] = y1p - (y1 * y2**2 * zp**2)
-    # F[0] = y1p - (y1 * y2**2 * zp)
+    if nonlinear_multiplier:
+        F[0] = y1p - (y1 * y2**2 * zp**2)
+    else:
+        F[0] = y1p - (y1 * y2 * zp)
     F[1] = y2p - (y1**2 * y2**2 - 3 * y2**2 * zp)
     F[2] = y1**2 * y2 - 1.0
 
@@ -28,11 +28,11 @@ def F(t, y, yp):
 if __name__ == "__main__":
     # time span
     t0 = 0
-    t1 = 2
+    t1 = 1
     t_span = (t0, t1)
 
     # tolerances
-    rtol = atol = 1e-4
+    rtol = atol = 1e-6
 
     # initial conditions
     y0 = np.array([1, 1, 0], dtype=float)
@@ -49,15 +49,15 @@ if __name__ == "__main__":
     # dae solution
     ##############
     start = time.time()
-    method = "BDF"
-    # method = "Radau"
+    # method = "BDF"
+    method = "Radau"
     sol = solve_dae(F, t_span, y0, yp0, atol=atol, rtol=rtol, method=method)
     end = time.time()
     print(f"elapsed time: {end - start}")
     t = sol.t
     y = sol.y
-    tp = t[1:]
-    yp = np.diff(y) / np.diff(t)
+    tp = t
+    yp = sol.yp
     success = sol.success
     status = sol.status
     message = sol.message
@@ -69,17 +69,14 @@ if __name__ == "__main__":
     print(f"nlu: {sol.nlu}")
 
     # errors
-    # assert_allclose(y[0], np.exp(t), rtol=1e-8)
-    # assert_allclose(y[1], np.exp(-2 * t), rtol=1e-8)
-    # assert_allclose(y[2], np.exp(2 * t), rtol=1e-8)
     dt = t[1] - t[0]
     error_y1 = np.linalg.norm((y[0] - np.exp(t)) * dt)
     error_y2 = np.linalg.norm((y[1] - np.exp(-2 * t)) * dt)
     # Note: We have an expected order reduction of h here since we control the
     #       error of y[2] instead of yp[2].
-    # error_y3 = np.linalg.norm((yp[2] - np.exp(2 * tp)) * dt)
-    error_y3 = np.linalg.norm((yp[2] - np.exp(2 * tp)) * dt**2)
-    print(f"error: [{error_y1}, {error_y2}, {error_y3}]")
+    # error_z = np.linalg.norm((yp[2] - np.exp(2 * tp)) * dt)
+    error_z = np.linalg.norm((yp[2] - np.exp(2 * tp)) * dt**2)
+    print(f"error: [{error_y1}, {error_y2}, {error_z}]")
 
     # visualization
     fig, ax = plt.subplots()
