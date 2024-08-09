d515453c0a0f536c8fb11250ce15ef83a88d51df
Jonas Breuling <jonas.breuling@inm.uni-stuttgart.de>
2024-08-09 07:02:14 +0200

    Weissinger benchmark with IDA results.

--- examples/daes/stiff_transistor_amplifier.py
+++ examples/daes/stiff_transistor_amplifier.py
@@ -5,13 +5,15 @@ from scipy_dae.integrate import solve_dae, consistent_initial_conditions
 
 
 """This example shows how to solve a stiff differential algebraic equation (DAE) 
-that describes an electrical circuit, see [1]_ and [2]_. 
+that describes an electrical circuit, see [1]_ and [2]_. The parameters and the 
+exponential model for the transistor are discussed in [3]_. 
 
 References
 ----------
 .. [1] E. Hairer, G. Wanner, "Solving Ordinary Differential Equations II:
         Stiff and Differential-Algebraic Problems", p. 377.
 .. [2] https://www.mathworks.com/help/matlab/math/solve-stiff-transistor-dae.html
+.. [3] https://doi.org/10.1007/BF01398915
 """
 
 # Problem parameters
@@ -20,8 +22,10 @@ R0 = 1000
 Ri = 9000
 R1, R2, R3, R4, R5 = 9000 * np.ones(5)
 alpha = 0.99
+beta = 1e-6
+Uf = 0.026
 Ue = lambda t: 0.4 * np.sin(200 * np.pi * t)  
-f = lambda U: 1e-6 * (np.exp(U / 0.026) - 1)
+f = lambda U: beta * (np.exp(U / Uf) - 1)
 C1, C2, C3 = 1e-6 * np.arange(1, 4)
 
 # initial states
@@ -63,7 +67,6 @@ y0, up0, fnorm = consistent_initial_conditions(F, t0, y0, up0)
 print(f"u0: {y0}")
 print(f"up0: {up0}")
 print(f"fnorm: {fnorm}")
-exit()
 
 # solver options
 atol = rtol = 1e-6
@@ -74,7 +77,7 @@ sol = solve_dae(F, t_span, y0, up0, atol=atol, rtol=rtol, method=method, t_eval=
 end = time.time()
 print(f"elapsed time: {end - start}")
 t = sol.t
-u = sol.y
+y = sol.y
 success = sol.success
 status = sol.status
 message = sol.message
@@ -85,12 +88,34 @@ print(f"nfev: {sol.nfev}")
 print(f"njev: {sol.njev}")
 print(f"nlu: {sol.nlu}")
 
+import numpy as np
+import sys
+from pathlib import Path
+
+header = "t, Ue, U1, U2, U3, U4, U5"
+
+data = np.vstack((
+    t[None, :],
+    Ue(t)[None, :],
+    y,
+)).T
+
+path = Path(sys.modules["__main__"].__file__)
+
+np.savetxt(
+    path.parent / (path.stem + ".txt"),
+    data,
+    delimiter=", ",
+    header=header,
+    comments="",
+)
+
 # visualization
 fig, ax = plt.subplots()
 ax.set_xlabel("t")
 ax.set_title("One transistor amplifier problem")
 ax.plot(t, Ue(t), label="input voltage Ue(t)")
-ax.plot(t, u[4], label="output voltage U5(t)")
+ax.plot(t, y[4], label="output voltage U5(t)")
 ax.grid()
 ax.legend()
 plt.show()
